df8b7c77e9da2118fc363ff3b6e2ebd1f0449759
jevalen4868 <jeremyvalenzuela@J01.local>
2016-06-16 23:24:26 -0500

    Thyme leaf v1

--- spittr-thymeleaf/src/spittr/web/WebConfig.java
+++ spittr-thymeleaf/src/spittr/web/WebConfig.java
@@ -1,5 +1,7 @@
 package spittr.web;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,21 +12,49 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("spittr.web")
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+    
+    private ApplicationContext applicationContext;
+    
+    public void setApplicationContext(ApplicationContext applicationContext) {
+	this.applicationContext = applicationContext;
+    }
+    
     @Bean
     public ViewResolver viewResolver() {
-	InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-	resolver.setPrefix("/WEB-INF/views/");
-	resolver.setSuffix(".jsp");
-	resolver.setExposeContextBeansAsAttributes(true);
-	return resolver;
+	ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	viewResolver.setTemplateEngine(templateEngine());
+	viewResolver.setCharacterEncoding("UTF-8");
+	return viewResolver;
     }
 
+    @Bean
+    public TemplateEngine templateEngine() {
+	SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	templateEngine.setEnableSpringELCompiler(true);
+	templateEngine.setTemplateResolver(templateResolver());
+	return templateEngine;
+    }
+
+    private ITemplateResolver templateResolver() {
+	SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	templateResolver.setApplicationContext(applicationContext);
+	templateResolver.setPrefix("/WEB-INF/templates/");
+	templateResolver.setSuffix(".html");
+	templateResolver.setTemplateMode(TemplateMode.HTML);
+	return templateResolver;
+    }
+    
     @Bean
     public MessageSource messageSource() {
 	ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
@@ -32,13 +62,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	messageSource.setCacheSeconds(10);
 	return messageSource;
     }
-    
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      // TODO Auto-generated method stub
-      super.addResourceHandlers(registry);
+	// TODO Auto-generated method stub
+	super.addResourceHandlers(registry);
     }
-    
+
     /*
      * This enables the server to return static assets without the servlet.
      * (non-Javadoc)
